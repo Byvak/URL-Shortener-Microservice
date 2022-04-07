204baf2e7aac5899c6ccb848d1f523c7a6a031ef
Burnny Kibonge <kibonge.burnny@gmail.com>
2022-04-07 14:48:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 const dbUri = process.env.MONGO_URI;
+var Schema = mongoose.Schema;
 
 mongoose.createConnection(dbUri, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     if (err) {
