0948e6f9be5590f78ad2319df2a710473ded2637
Burnny Kibonge <kibonge.burnny@gmail.com>
2022-04-23 19:02:00 +0100

    Update counter_entity.js

--- models/entities/counter_entity.js
+++ models/entities/counter_entity.js
@@ -1,9 +1,3 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-const counterEntity = new Schema({
-    _id: { type: String, required: true },
-    seq: { type: Number, default: 0 },
-}, { bufferCommands: false });
-
-module.exports = mongoose.model("Counter", counterEntity);
