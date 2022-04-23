bc99675a4a52849c00880eea9729aca35120e1f5
Burnny Kibonge <kibonge.burnny@gmail.com>
2022-04-23 19:03:07 +0100

    Update counter_dao.js

--- models/counter_dao.js
+++ models/counter_dao.js
@@ -5,6 +5,7 @@ const Counter = mongoose.model("Counter", counter_entity);
 
 module.exports.findByIdAndUpdate = function (id, callback) {
     Counter.findByIdAndUpdate(
+        { _id: id },
             } else {
                 callback(doc);
             }
