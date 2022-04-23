96e19391e4340ff06fe1c4e28b72830894e0e300
Burnny Kibonge <kibonge.burnny@gmail.com>
2022-04-23 19:02:41 +0100

    Update counter_dao.js

--- models/counter_dao.js
+++ models/counter_dao.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const counter_entity = require('./entities/counter_entity').CounterEntity();
 const Counter = mongoose.model("Counter", counter_entity);
+
         }
     );
 }
