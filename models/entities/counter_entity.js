deff5fb07bc8baba8d15e0035ff6b175c3ec2471
Burnny Kibonge <kibonge.burnny@gmail.com>
2022-04-23 19:02:06 +0100

    Update counter_entity.js

--- models/entities/counter_entity.js
+++ models/entities/counter_entity.js
@@ -1,3 +1,9 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+module.exports.CounterEntity = function CounterEntity() {
+    return new Schema({
+        _id: { type: String, required: true },
+        seq: { type: Number, default: 0 },
+    });
+}
